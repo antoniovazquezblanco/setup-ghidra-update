b95d82ea1dbea9ca29e432d44d117addcce01afe
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2025-12-08 21:06:48 +0100

    Add setVersionsInMatrixVariable test.

--- src/workflow_helper.test.ts
+++ src/workflow_helper.test.ts
@@ -1,5 +1,8 @@
 import * as yaml from "yaml";
-import { getSetupGhidraVersionOption } from "./workflow_helper.js";
+import {
+  getSetupGhidraVersionOption,
+  setVersionsInMatrixVariable,
+} from "./workflow_helper.js";
 
 describe("workflow_helper", () => {
   describe("getSetupGhidraVersionOption", () => {
@@ -32,6 +35,37 @@ describe("workflow_helper", () => {
 
       const result = await getSetupGhidraVersionOption(step);
       expect(result).toBeNull();
+    });
+  });
+
+  describe("setVersionsInMatrixVariable", () => {
+    it("should set versions in matrix variable", async () => {
+      const variable = new yaml.Pair(
+        new yaml.Scalar("versions"),
+        new yaml.YAMLSeq(),
+      );
+      const versions = ["10.2.3", "10.3.0"];
+
+      await setVersionsInMatrixVariable(variable, versions);
+
+      const seq = variable.value as yaml.YAMLSeq<yaml.Scalar<string>>;
+      expect(seq.items.length).toBe(2);
+      expect(seq.items[0].value).toBe("10.2.3");
+      expect(seq.items[1].value).toBe("10.3.0");
+    });
+
+    it("should handle empty versions array", async () => {
+      // Setup
+      const old_vers = new yaml.YAMLSeq();
+      old_vers.add(new yaml.Scalar("old"));
+      const variable = new yaml.Pair(new yaml.Scalar("versions"), old_vers);
+
+      // Action
+      await setVersionsInMatrixVariable(variable, []);
+
+      // Check
+      const seq = variable.value as yaml.YAMLSeq;
+      expect(seq.items.length).toBe(0);
     });
   });
 });
