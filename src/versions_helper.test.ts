b2b47faada7204ad224a02c6308ed163d10f166f
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2025-12-08 20:11:04 +0100

    Add a version sorting test.

--- src/versions_helper.test.ts
+++ src/versions_helper.test.ts
@@ -13,5 +13,29 @@ describe("versions_helper", () => {
       const sorted = sortVersionArray(versions);
       expect(sorted).toEqual(["1.0.0"]);
     });
+
+    it("should sort version strings correctly", () => {
+      const versions = [
+        "1.2",
+        "1.1.1",
+        "1.1.0",
+        "1.3.0",
+        "1.0.0",
+        "2",
+        "2.1.0",
+        "2.2",
+      ];
+      const sorted = sortVersionArray(versions);
+      expect(sorted).toEqual([
+        "1.0.0",
+        "1.1.0",
+        "1.1.1",
+        "1.2",
+        "1.3.0",
+        "2",
+        "2.1.0",
+        "2.2",
+      ]);
+    });
   });
 });
